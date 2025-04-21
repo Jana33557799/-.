8602d43d7c1e7f6086ea10c53e62e48865cdc122
بطاقات الشكر جامعة طيبة <jana34346@gmail.com>
2025-04-21 22:17:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function generateCertificate() {
     ctx.textAlign = 'center';
     ctx.fillStyle = '#333';
 
-    // اسم عضو هيئة التدريس - رفع إلى y = 260
+    // اسم عضو هيئة التدريس - رفع إلى y = 270
     ctx.font = 'bold 30px Cairo';
-    ctx.fillText(teacherName || ' ', canvas.width / 2, 260);
+    ctx.fillText(teacherName || ' ', canvas.width / 2, 270);
 
     // اسم الطالب - تحت المربع بثلاث أسطر تقريباً
     ctx.font = 'bold 34px Cairo';
